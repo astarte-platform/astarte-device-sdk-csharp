89ef6ba1c4b2c98b2131c2169d73fb3e9bb837cb
Nedim Tokic <nedim.tokic@secomind.com>
2022-11-11 10:25:23 +0100

    Connect to astarte
    Connect on mqtt broker using SDK

--- Crypto.cs
+++ Crypto.cs
@@ -1,5 +1,8 @@
 ﻿using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -17,22 +20,22 @@ namespace AstarteDeviceSDKCSharp
 
             if (!File.Exists(Path.Combine(cryptoStoreDir, fileName)))
             {
-                string privateKey = new(PemEncoding.Write("PRIVATE KEY",ecdsa.ExportECPrivateKey()).ToArray());
-                File.WriteAllText(Path.Combine(cryptoStoreDir, fileName),privateKey);
+                string privateKey = new(PemEncoding.Write("PRIVATE KEY", ecdsa.ExportECPrivateKey()).ToArray());
+                File.WriteAllText(Path.Combine(cryptoStoreDir, fileName), privateKey);
             }
             else
             {
-                string publicKey = File.ReadAllText(
-                        Path.Combine(cryptoStoreDir, fileName)).ToString()
-                        .Replace("-----BEGIN PRIVATE KEY-----", "")
-                        .Replace("-----END PRIVATE KEY-----", "").Replace("\n", "");
-                ecdsa.ImportECPrivateKey(Convert.FromBase64String(publicKey), out _);
+                string privateKey = File.ReadAllText(
+                Path.Combine(cryptoStoreDir, fileName)).ToString()
+               .Replace("-----BEGIN PRIVATE KEY-----", "")
+               .Replace("-----END PRIVATE KEY-----", "").Replace("\n", "");
+                ecdsa.ImportECPrivateKey(Convert.FromBase64String(privateKey), out _);
             }
 
             var cert = new CertificateRequest($"O=Devices,CN={realm}/{deviceId}", ecdsa, HashAlgorithmName.SHA256);
 
             byte[] pkcs10 = cert.CreateSigningRequest();
-            StringBuilder builder = new StringBuilder();
+            StringBuilder builder = new();
 
             builder.AppendLine("-----BEGIN CERTIFICATE REQUEST-----");
 
@@ -51,61 +54,126 @@ namespace AstarteDeviceSDKCSharp
             builder.AppendLine("-----END CERTIFICATE REQUEST-----");
             return builder.ToString();
         }
-
-
-        public void ImportDeviceCertificate(string certificate, string cryptoStoreDir)
+        public void ImportDeviceCertificate(string clientCrt, string cryptoStoreDir)
         {
+            var cert = new X509Certificate2(Convert.FromBase64String(clientCrt
+                        .Replace("-----BEGIN CERTIFICATE-----", "")
+                        .Replace("-----END CERTIFICATE-----", "")
+                        .Replace("\n", "")));
 
-            var cert = new X509Certificate(Convert.FromBase64String(certificate));
-            //File.WriteAllText(Path.Combine(cryptoStoreDir, "device.crt"), 
-            //           PemEncoding.Write("PRIVATE KEY",cert.GetRawCertData().ToArray());
+            File.WriteAllText(Path.Combine(cryptoStoreDir, "device.crt"), 
+                new(PemEncoding.Write("CERTIFICATE", cert.GetRawCertData()).ToArray()));
+        }
+        //public async Task SetupMqttAsync(string realm, string deviceId, string cryptoStoreDir)
+        //{
+
+        //    var mqttFactory = new MqttFactory();
+        //    ECDsa ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+        //    string privateKey = File.ReadAllText(
+        //            Path.Combine(cryptoStoreDir, "device.key")).ToString()
+        //            .Replace("-----BEGIN PRIVATE KEY-----", "")
+        //            .Replace("-----END PRIVATE KEY-----", "").Replace("\n", "");
+        //    ecdsa.ImportECPrivateKey(Convert.FromBase64String(privateKey), out _);
+
+        //    X509Certificate2 caCert = new X509Certificate2(File.ReadAllBytes(cryptoStoreDir + @"\device.crt"));
+        //    X509Certificate2 caCertWithKey = caCert.CopyWithPrivateKey(ecdsa);
+        //    X509Certificate2 clCertPFX = new X509Certificate2(caCertWithKey.Export(X509ContentType.Pfx));
+
+        //    using (var mqttClient = mqttFactory.CreateMqttClient())
+        //    {
+
+        //        var tlsOptions = new MqttClientOptionsBuilderTlsParameters
+        //        {
+        //            UseTls = true,
+        //            Certificates = new List<X509Certificate>
+        //            {
+        //              clCertPFX
+        //            },
+        //            IgnoreCertificateChainErrors = true,
+        //            IgnoreCertificateRevocationErrors = true,
+        //            SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+        //            AllowUntrustedCertificates = true,
+
+        //        };
+
+        //        var mqttClientOptions = new MqttClientOptionsBuilder()
+        //            .WithClientId(Guid.NewGuid().ToString())
+        //            .WithTcpServer("localhost", 8883)
+        //            .WithTls(tlsOptions)
+        //            .WithCleanSession()
+        //            .Build();
+
+        //        try
+        //        {
+        //            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+
+        //        var introspectionMessage = new MqttApplicationMessageBuilder()
+        //         .WithTopic($"{realm}/{deviceId}")
+        //         .WithPayload("org.astarte-platform.genericsensors.Values:1:0")
+        //         .Build();
+
+        //        await mqttClient.PublishAsync(introspectionMessage);
+
+        //        Event e = new Event
+        //        {
+        //            v = 19.5
+        //        };
+
+
+        //        MemoryStream ms = new MemoryStream();
+        //        using (BsonDataWriter writer = new BsonDataWriter(ms))
+        //        {
+        //            JsonSerializer serializer = new JsonSerializer();
+        //            serializer.Serialize(writer, e);
+        //        }
+
+        //        var a = ms.ToArray();
+
+        //        try
+        //        {
+        //            var applicationMessage = new MqttApplicationMessageBuilder()
+        //                              .WithTopic($"{realm}/{deviceId}/org.astarte-platform.genericsensors.Values/test/value")
+        //                              .WithPayload(a)
+        //                              .Build();
+        //            await mqttClient.PublishAsync(applicationMessage);
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message.ToString());
+        //        }
+
+        //        mqttClient.SubscribeAsync(
+        //           new MqttTopicFilterBuilder()
+        //             .WithTopic($"{realm}/{deviceId}/#")
+        //             .WithExactlyOnceQoS()
+        //             .Build())
+        //             .GetAwaiter().GetResult();
+
+        //        await mqttClient.DisconnectAsync();
+
+        //        Console.WriteLine("MQTT application message is published.");
+        //    }
+        //}
+        //public class Certificate
+        //{
+        //    public Data data { get; set; }
+        //}
+        public class Data
+        {
+            public string client_crt { get; set; } = string.Empty;
 
         }
-
-
-        public async Task SetupMqttAsync(string realm, string deviceId, string cryptoStoreDir)
-        {
-
-            var mqttFactory = new MqttFactory();
-
-            using (var mqttClient = mqttFactory.CreateMqttClient())
-            {
-
-                var tlsOptions = new MqttClientOptionsBuilderTlsParameters
-                {
-                    UseTls = true,
-                    Certificates = new List<X509Certificate>
-                    {
-                        new X509Certificate(cryptoStoreDir + "device.crt")
-                    },
-                    IgnoreCertificateChainErrors = true,
-                    IgnoreCertificateRevocationErrors = true,
-                    SslProtocol = System.Security.Authentication.SslProtocols.Tls,
-
-                };
-
-                var mqttClientOptions = new MqttClientOptionsBuilder()
-                    .WithTcpServer("mqtts://localhost:8883/")
-                    .WithTls(tlsOptions)
-                    .WithCleanSession()
-                    .Build();
-
-                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-                var applicationMessage = new MqttApplicationMessageBuilder()
-                    .WithTopic($"{realm}/{deviceId}/#")
-                    .WithPayload("19.5")
-                    .Build();
-
-                await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
-
-                await mqttClient.DisconnectAsync();
-
-                Console.WriteLine("MQTT application message is published.");
-            }
-        }
-
+   
     }
 }
+
 
 
--- PairingHandler.cs
+++ PairingHandler.cs
@@ -1,6 +1,15 @@
-﻿using System.Diagnostics;
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Builder;
+using JWT.Serializers;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Net.Http.Headers;
-
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace AstarteDeviceSDKCSharp
 {
@@ -9,44 +18,201 @@ namespace AstarteDeviceSDKCSharp
         public async Task ObtainDeviceCertificate(string deviceId, string realm, string credentialsSecret, string pairingBaseURL, string cryptoStoreDir)
         {
 
-            var myCrypto = new Crypto();
-            var csr = myCrypto.GenerateCsr(realm, deviceId, cryptoStoreDir);
+            Crypto myCrypto = new();
+            // Get a CSR
+            string csr = myCrypto.GenerateCsr(realm, deviceId, cryptoStoreDir);
 
-            var client = new HttpClient();
+            // Prepare the Pairing API request
+            HttpClient client = new();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", credentialsSecret);
             try
             {
-                HttpContent content = new StringContent("{'data': {'csr': " + csr + "}}");
+
+                var json = JsonConvert.SerializeObject(new CertificateRequest() { data = new csrData() { csr = csr } });
+                HttpContent content = new StringContent(json);
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 var response = await client.PostAsync(pairingBaseURL + $"/v1/{realm}/devices/{deviceId}/protocols/astarte_mqtt_v1/credentials", content);
+
+                var certificate = JsonConvert.DeserializeObject<Certificate>(await response.Content.ReadAsStringAsync());
+                
+                // Save certificate from response
+                if (certificate != null)
+                {
+                    myCrypto.ImportDeviceCertificate(certificate.data.client_crt, cryptoStoreDir);
+                }
 
             }
             catch (Exception e)
             {
                 Debug.WriteLine(e);
             }
+        }
 
+        public async Task<string> ObtainDeviceTransportInformationAsync(string deviceId, string realm, string credentialsSecret, string pairingBaseURL)
+        {
+            // Prepare the Pairing API request
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", credentialsSecret);
+            try
+            {
+                var response = await client.GetAsync(pairingBaseURL + $"/v1/{realm}/devices/{deviceId}");
+
+                return await response.Content.ReadAsStringAsync();
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return "";
+            }
 
         }
 
-        public async Task RegisterDevice(string deviceId, string realm, string credentialsSecret, string pairingBaseURL)
+        public async Task<string> RegisterDeviceWithPrivateKey(string deviceId, string realm, string privateKeyFile, string pairingBaseURL)
+        {
+
+            return await RegisterDevice(deviceId,realm, RegisterDeviceHeadersWithPrivateKey(privateKeyFile),pairingBaseURL);
+        }
+
+        public async Task<string> RegisterDeviceWithJwtToken(string deviceId, string realm, string jwtToken, string pairingBaseURL)
+        {
+            return await RegisterDevice(deviceId, realm, RegisterDeviceHeadersWithJwtToken(jwtToken), pairingBaseURL);
+        }
+
+        private AuthenticationHeaderValue RegisterDeviceHeadersWithPrivateKey(string privateKeyfile)
+        {
+            var headers = new AuthenticationHeaderValue("Bearer", GenerateToken(privateKeyfile,"pairing"));
+            return headers;
+        }
+        private AuthenticationHeaderValue RegisterDeviceHeadersWithJwtToken(string jwtToken)
+        {
+            var headers = new AuthenticationHeaderValue("Bearer", jwtToken);
+            return headers;
+        }
+        private string GenerateToken(string privateKeyFile, string type = "appengine", List<string>? authPaths = null, int expiry = 30)
+        {
+
+            try
+            {
+                if (authPaths == null)
+                {
+                    authPaths.Add(".*::.*");
+                }
+
+                // reading the content of a private key PEM file, PKCS8 encoded 
+                string privateKeyPem = File.ReadAllText(privateKeyFile);
+
+                // keeping only the payload of the key 
+                privateKeyPem = privateKeyPem.Replace("-----BEGIN PRIVATE KEY-----", "");
+                privateKeyPem = privateKeyPem.Replace("-----END PRIVATE KEY-----", "");
+
+                byte[] privateKeyRaw = Convert.FromBase64String(privateKeyPem);
+
+                // creating the RSA key 
+                RSACryptoServiceProvider provider = new ();
+                provider.ImportPkcs8PrivateKey(new ReadOnlySpan<byte>(privateKeyRaw), out _);
+                RsaSecurityKey rsaSecurityKey = new (provider);
+
+
+                List<string> realAuthPaths = new();
+
+                if (type== "channels" && authPaths == new List<string>() {".*::.*"})
+                {
+                    realAuthPaths.Add("JOIN::.*");
+                    realAuthPaths.Add("WATCH::.*");
+                }
+                else
+                {
+                    realAuthPaths.Add(".*::.*");
+                }
+
+                // Generating the token 
+                var now = DateTime.UtcNow;
+
+                var claims = new[] {
+                    new Claim( "appengine",  "a_aea"),
+                    new Claim( "realm",  "a_rma"),
+                    new Claim( "housekeeping",  "a_ha"),
+                    new Claim( "channels",  "a_ch"),
+                    new Claim( "pairing",  "a_pa"),
+                };
+
+                var handler = new JwtSecurityTokenHandler();
+
+                var token = new JwtSecurityToken
+                (   null,
+                    null,
+                    claims,
+                    now.AddMilliseconds(-30),
+                    now.AddMinutes(60),
+                    new SigningCredentials(rsaSecurityKey, SecurityAlgorithms.RsaSha256)
+                );
+
+                // handler.WriteToken(token) returns the token!
+                 return handler.WriteToken(token);
+
+            }
+
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                return "";
+            }
+        }
+
+        private async Task<string> RegisterDevice(string deviceId, string realm, AuthenticationHeaderValue header, string pairingBaseURL)
         {
             var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", credentialsSecret);
+            client.DefaultRequestHeaders.Authorization = header;
             try
             {
                 HttpContent content = new StringContent("{'data': {'hw_id': " + deviceId + "}}");
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                var response = await client.PostAsync(pairingBaseURL + $"/v1/{realm}/agent/devices", content);
+                HttpResponseMessage response = await client.PostAsync(pairingBaseURL + $"/v1/{realm}/agent/devices", content);
 
+                if (response.StatusCode == HttpStatusCode.Unauthorized || response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    throw new Exception(await response.Content.ReadAsStringAsync());
+                }
+                else if (response.StatusCode == HttpStatusCode.UnprocessableEntity)
+                {
+                    throw new Exception(await response.Content.ReadAsStringAsync());
+                }
+                else if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    throw new Exception(await response.Content.ReadAsStringAsync());
+                }
+
+                dynamic credential = await response.Content.ReadAsStringAsync();
+
+                return credential.credential_secret;
             }
             catch (Exception e)
             {
                 Debug.WriteLine(e);
+                return "";
             }
+        }
+        public class Certificate
+        {
+
+            public Data data { get; set; } = new Data();
+        }
+        public class Data
+        {
+            public string client_crt { get; set; } = string.Empty;
+        }
+        public class CertificateRequest
+        {
+            public csrData data { get; set; } = new csrData();
+        }
+        public class csrData
+        {
+            public string csr { get; set; } = string.Empty;
         }
 
 
 
     }
 }
+
