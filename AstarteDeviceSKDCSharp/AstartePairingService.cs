8acdd72da4cecd538d55973a8fa2d81417d32e9a
Nedim Tokic <nedim.tokic@secomind.com>
2023-01-24 08:54:45 +0100

    Generate device ID and register with private key or JWT token

--- AstarteDeviceSKDCSharp/AstartePairingService.cs
+++ AstarteDeviceSKDCSharp/AstartePairingService.cs
@@ -22,9 +22,13 @@ using AstarteDeviceSDK.Protocol;
 using AstarteDeviceSDKCSharp.Crypto;
 using AstarteDeviceSDKCSharp.Transport;
 using AstarteDeviceSDKCSharp.Utilities;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
@@ -32,7 +36,7 @@ namespace AstarteDeviceSDKCSharp
 {
     public class AstartePairingService
     {
-        private Uri _pairingUrl;
+        private readonly Uri _pairingUrl;
         private readonly string _astarteRealm;
         private readonly HttpClient _httpClient;
 
@@ -52,7 +56,7 @@ namespace AstarteDeviceSDKCSharp
         }
 
 
-        public async Task<List<AstarteTransport>> ReloadTransports(string credentialSecret,
+        internal async Task<List<AstarteTransport>> ReloadTransports(string credentialSecret,
         AstarteCryptoStore astarteCryptoStore, string deviceId)
         {
             List<AstarteTransport> transports = new();
@@ -116,7 +120,6 @@ namespace AstarteDeviceSDKCSharp
                 throw new AstartePairingException(ex.Message);
             }
 
-
         }
 
         public async Task<X509Certificate2> RequestNewCertificate
@@ -176,14 +179,21 @@ namespace AstarteDeviceSDKCSharp
 
                 try
                 {
-                    X509Certificate2 newCertificate = new X509Certificate2
-                    (Convert.FromBase64String(certificate.Data.ClientCrt
-                     .Replace("-----BEGIN CERTIFICATE-----", "")
-                     .Replace("-----END CERTIFICATE-----", "")
-                     .Replace("\n", "")));
+                    if (certificate != null)
+                    {
+                        X509Certificate2 newCertificate = new X509Certificate2
+                        (Convert.FromBase64String(certificate.Data.ClientCrt
+                        .Replace("-----BEGIN CERTIFICATE-----", "")
+                        .Replace("-----END CERTIFICATE-----", "")
+                        .Replace("\n", "")));
 
-                    astarteCryptoStore.SaveCertificateIfNotExist(newCertificate);
-                    return newCertificate;
+                        astarteCryptoStore.SaveCertificateIfNotExist(newCertificate);
+                        return newCertificate;
+                    }
+                    else
+                    {
+                        throw new AstartePairingException("Certificate is null");
+                    }
                 }
                 catch (CryptographicException ex)
                 {
@@ -195,5 +205,182 @@ namespace AstarteDeviceSDKCSharp
                 throw new AstartePairingException("Failure in calling Pairing API", ex);
             }
         }
+
+        /// <summary>
+        /// Registers a Device against an Astarte instance/realm with a Private Key
+        /// </summary>
+        /// <param name="deviceId">The Device ID to register.</param>
+        /// <param name="privateKeyFile">Path to the Private Key file for the Realm.
+        /// It will be used to Authenticate against Pairing API.</param>
+        /// <returns>Returns the Credentials secret for the Device</returns>
+        public async Task<string> RegisterDeviceWithPrivateKey(string deviceId, string privateKeyFile)
+        {
+            return await RegisterDevice(deviceId, RegisterDeviceHeadersWithPrivateKey(privateKeyFile));
+        }
+
+        /// <summary>
+        /// Registers a Device against an Astarte instance/realm with a JWT Token
+        /// </summary>
+        /// <param name="deviceId">The Device ID to register.</param>
+        /// <param name="jwtToken">A JWT Token to Authenticate against Pairing API. 
+        /// The token must have access to Pairing API and to the agent API paths.</param>
+        /// <returns>Returns the Credentials secret for the Device</returns>
+        public async Task<string> RegisterDeviceWithJwtToken(string deviceId, string jwtToken)
+        {
+            return await RegisterDevice(deviceId, RegisterDeviceHeadersWithJwtToken(jwtToken));
+        }
+
+        private AuthenticationHeaderValue RegisterDeviceHeadersWithPrivateKey(string privateKeyfile)
+        {
+            return new AuthenticationHeaderValue("Bearer", GenerateToken(privateKeyfile));
+        }
+
+        private AuthenticationHeaderValue RegisterDeviceHeadersWithJwtToken(string jwtToken)
+        {
+            return new AuthenticationHeaderValue("Bearer", jwtToken);
+        }
+
+        private string GenerateToken(string privateKeyFile, int expiry = 30)
+        {
+            List<string> authPaths = new();
+            authPaths.Add(".*::.*");
+
+            string privateKey;
+            try
+            {
+                privateKey = File.ReadAllText(
+                privateKeyFile).ToString()
+                .Replace("-----BEGIN EC PRIVATE KEY-----", "")
+                .Replace("-----END EC PRIVATE KEY-----", "")
+                .Replace("\n", "");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                throw new AstartePairingException("Private key file or directory not found", e);
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new AstartePairingException("Private key file not found", e);
+            }
+            catch (IOException e)
+            {
+                throw new AstartePairingException("Can not access to file", e);
+            }
+            catch (Exception e)
+            {
+                throw new AstartePairingException("Private key read error", e);
+            }
+
+            // creating the key 
+            ECDsa ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+            try
+            {
+                ecdsa.ImportECPrivateKey(Convert.FromBase64String(privateKey), out _);
+            }
+            catch (CryptographicException e)
+            {
+                throw new AstartePairingException("Import private key error", e);
+            }
+
+            ECDsaSecurityKey rsaSecurityKey = new(ecdsa);
+
+            List<string> realAuthPaths = new();
+            realAuthPaths.Add("JOIN::.*");
+            realAuthPaths.Add("WATCH::.*");
+
+            // Generating the token 
+            var now = DateTime.UtcNow;
+            var startDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            JArray jRealAuthPaths = new(realAuthPaths);
+
+            var claims = new[] {
+                    new Claim( "iat", ((long)(now - startDate).TotalSeconds).ToString(), ClaimValueTypes.Integer64),
+                    new Claim( "a_pa",jRealAuthPaths.ToString(),JsonClaimValueTypes.JsonArray),
+                };
+
+            var handler = new JwtSecurityTokenHandler();
+
+            JwtSecurityToken token;
+            try
+            {
+                token = new JwtSecurityToken(
+                                   null,
+                                   null,
+                                   claims,
+                                   null,
+                                   now.AddSeconds(expiry),
+                                   new SigningCredentials(rsaSecurityKey, SecurityAlgorithms.EcdsaSha256)
+                               );
+            }
+            catch (ArgumentException e)
+            {
+                throw new AstartePairingException("Token parameters error", e);
+            }
+
+            try
+            {
+                return handler.WriteToken(token);
+            }
+            catch (ArgumentNullException e)
+            {
+                throw new AstartePairingException("Token is null", e);
+            }
+            catch (ArgumentException e)
+            {
+                throw new AstartePairingException("Token is not valid", e);
+            }
+            catch (Exception e)
+            {
+                throw new AstartePairingException("Token serialization failed", e);
+            }
+
+        }
+
+        private async Task<string> RegisterDevice(string deviceId, AuthenticationHeaderValue header)
+        {
+            string credentialSecret = "";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = header;
+
+            JObject payLoad;
+            try
+            {
+                payLoad = new(
+                    new JProperty("data",
+                      new JObject(
+                        new JProperty("hw_id", deviceId)))
+                );
+            }
+            catch (JsonException ex)
+            {
+                throw new AstartePairingException("Could not generate the JSON Request Payload", ex);
+            }
+
+            HttpContent content = new StringContent(payLoad.ToString());
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await client.PostAsync(_pairingUrl + $"/{_astarteRealm}/agent/devices", content);
+
+            if (!response.IsSuccessStatusCode || response.Content == null)
+            {
+                throw new AstartePairingException(
+                    "Request to device register API failed with "
+                        + response.StatusCode
+                        + ". Returned body is "
+                        + (response.Content != null ? await response.Content.ReadAsStringAsync() : "empty"));
+            }
+
+            dynamic? credential = JsonConvert.DeserializeObject<object>(await response.Content.ReadAsStringAsync());
+            if (credential != null)
+            {
+                credentialSecret = credential.data.credentials_secret;
+            }
+
+            if (string.IsNullOrEmpty(credentialSecret))
+            {
+                throw new AstartePairingException("Failure in calling device register API");
+            }
+            return credentialSecret;
+        }
+
     }
 }
---
+++ AstarteDeviceSKDCSharp/Utilities/AstarteDeviceIdUtils.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * This file is part of Astarte.
+ *
+ * Copyright 2023 SECO Mind Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AstarteDeviceSDKCSharp.Utilities
+{
+    public class AstarteDeviceIdUtils
+    {
+        /// <summary>
+        /// Generate a device Id in a random way based on Guid.
+        /// </summary>
+        /// <returns>Generated device Id, using the standard Astarte Device ID encoding (base64 urlencoding without padding)</returns>
+        public static string GenerateId()
+        {
+            Uuid randomUUID = Uuid.NewUuid();
+
+            var stream = new MemoryStream();
+            var buffer = new BinaryWriter(stream);
+            buffer.Write(randomUUID.ToByteArray());
+            var encoded = Convert.ToBase64String(stream.ToArray()).TrimEnd('=');
+            return encoded;
+        }
+
+        /// <summary>
+        /// Generate a device Id based on Guid and a uniqueData.
+        /// </summary>
+        /// <param name="namespaceGuid">Guid namespace of the device_id</param>
+        /// <param name="uniqueData">Device unique data used to generate the device_id</param>
+        /// <returns>The generated device Id, using the standard Astarte Device ID encoding (base64 urlencoding without padding).</returns>
+        public static string GenerateId(Guid namespaceGuid, string uniqueData)
+        {
+            Uuid uuid5FromName = NameUUIDFromNamespaceAndString(namespaceGuid, uniqueData);
+
+            var stream = new MemoryStream();
+            var buffer = new BinaryWriter(stream);
+            buffer.Write(uuid5FromName.LeastSignificantBits);
+            buffer.Write(uuid5FromName.MostSignificantBits);
+            var encodedStr = Base64UrlEncoder.Encode(stream.ToArray().Reverse().ToArray());
+            return encodedStr;
+        }
+
+        private static Uuid NameUUIDFromNamespaceAndString(Uuid nameUuid, String data)
+        {
+            return NameUUIDFromNamespaceAndBytes(
+                nameUuid, Encoding.UTF8.GetBytes(data.ToString()));
+        }
+
+        private static Uuid NameUUIDFromNamespaceAndBytes(Uuid namespaceGuid, byte[] data)
+        {
+            var namespaceBytes = ToBytes(namespaceGuid);
+            byte[] hash;
+            using (var sha1 = SHA1.Create())
+            {
+                hash = new byte[namespaceBytes.Length + data.Length];
+                sha1.TransformBlock(namespaceBytes, 0, namespaceBytes.Length, hash, 0);
+                sha1.TransformFinalBlock(data, 0, data.Length);
+                byte[]? sha1Bytes = sha1.Hash;
+
+                if (sha1Bytes != null)
+                {
+                    sha1Bytes[6] &= 0x0f; /* clear version        */
+                    sha1Bytes[6] |= 0x50; /* set to version 5     */
+                    sha1Bytes[8] &= 0x3f; /* clear variant        */
+                    sha1Bytes[8] |= 0x80; /* set to IETF variant  */
+
+                    return FromBytes(sha1Bytes);
+                }
+                else
+                {
+                    throw new ArgumentException("Computed hash code is null");
+                }
+            }
+        }
+
+        private static Uuid FromBytes(byte[] data)
+        {
+            // Based on the private UUID(bytes[]) constructor
+            long msb = 0;
+            long lsb = 0;
+
+#pragma warning disable CS0675 // Bitwise-or operator used on a sign-extended operand
+            for (int i = 0; i < 8; i++) msb = (msb << 8) | (data[i] & 0xff);
+            for (int i = 8; i < 16; i++) lsb = (lsb << 8) | (data[i] & 0xff);
+#pragma warning restore CS0675 // Bitwise-or operator used on a sign-extended operand
+            return new Uuid(msb, lsb);
+        }
+
+        private static byte[] ToBytes(Uuid uuid)
+        {
+            // inverted logic of fromBytes()
+            byte[] outId = new byte[16];
+            long msb = uuid.MostSignificantBits;
+            long lsb = uuid.LeastSignificantBits;
+            for (int i = 0; i < 8; i++) outId[i] = (byte)((msb >> ((7 - i) * 8)) & 0xff);
+            for (int i = 8; i < 16; i++) outId[i] = (byte)((lsb >> ((15 - i) * 8)) & 0xff);
+            return outId;
+        }
+    }
+}
