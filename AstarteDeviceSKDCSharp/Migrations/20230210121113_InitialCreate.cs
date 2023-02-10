54e57144770668b047d557697d81e27c83c248e4
Osman Hadzic <osman.hadzic@secomind.com>
2023-02-10 13:31:54 +0100

    Setup entity framework and add Initial migration

---
+++ AstarteDeviceSKDCSharp/Migrations/20230210121113_InitialCreate.cs
@@ -0,0 +1,25 @@
+﻿// This file is part of Astarte.
+//
+// Copyright 2023 SECO Mind Srl
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AstarteDeviceSDKCSharp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
