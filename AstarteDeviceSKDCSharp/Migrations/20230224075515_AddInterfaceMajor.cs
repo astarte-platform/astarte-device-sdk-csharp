30810cb373dd32dfd1e0b7c9235806eda00502a8
Osman Hadzic <osman.hadzic@secomind.com>
2023-02-24 09:03:22 +0100

    Add a migration for a new field in AstarteGenericPropertyEntry

---
+++ AstarteDeviceSKDCSharp/Migrations/20230224075515_AddInterfaceMajor.cs
@@ -0,0 +1,32 @@
+﻿// This file is part of Astarte.
+//
+// Copyright 2023 SECO Mind Srl
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AstarteDeviceSDKCSharp.Migrations
+{
+    public partial class AddInterfaceMajor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InterfaceMajor",
+                table: "AstarteGenericProperties",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InterfaceMajor",
+                table: "AstarteGenericProperties");
+        }
+    }
+}
